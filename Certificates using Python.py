6bc522673322d7aee6cedfe8b4c67bf2427491d1
Ali Hassan <alihasanjatoi@gmail.com>
2021-07-17 22:08:27 +0500

    Create Certificates using Python.py

---
+++ Certificates using Python.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw, ImageFont
+
+for name in names:
+          
+        # adjust the position according to 
+        # your sample
+        text_y_position = 900 
+   
+        # opens the image
+        img = Image.open(certificate, mode ='r')
+          
+        # gets the image width
+        image_width = img.width
+          
+        # gets the image height
+        image_height = img.height 
+   
+        # creates a drawing canvas overlay 
+        # on top of the image
+        draw = ImageDraw.Draw(img)
+   
+        # gets the font object from the 
+        # font file (TTF)
+        font = ImageFont.truetype(
+            font_path,
+            200 # change this according to your needs
+        )
+   
+        # fetches the text width for 
+        # calculations later on
+        text_width, _ = draw.textsize(name, font = font)
