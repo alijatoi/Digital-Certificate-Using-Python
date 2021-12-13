8dde794d5167201f9fec7083e0271ca143f407b4
Ali Hassan <alihasanjatoi@gmail.com>
2021-12-13 15:56:30 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+import os
+df = pd.read_csv('D:/Digital-Certificate-Using-Python-main/list.csv')
+
+font = ImageFont.truetype('arial.ttf',120)
+for index,j in df.iterrows():
+    img = Image.open('certificate.jpg')
+    draw = ImageDraw.Draw(img)
+    draw.text(xy=(1580,950),text='{}'.format(j['name']),fill=(0,0,0),font=font)
+    img.save('pictures/{}.jpg'.format(j['name']))
+
+
